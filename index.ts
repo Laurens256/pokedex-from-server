0b283f013c9bf687c7963530508dbaf2e36d15ab
Laurens256 <duinlaurens2@gmail.com>
2023-03-20 11:29:06 +0100

    express setup

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from 'express';
+import path from 'path';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.engine(
+	'hbs',
+	engine({
+		layoutsDir: `${path.join(__dirname)}/views`,
+		partialsDir: `${path.join(__dirname)}/views/partials`,
+		defaultLayout: 'main',
+		extname: '.hbs'
+	})
+);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.render('splash', { title: 'Home' });
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
