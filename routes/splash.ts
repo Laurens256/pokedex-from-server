35694641a79988f081f7aaf5980566671be12d41
Laurens256 <duinlaurens2@gmail.com>
2023-03-20 12:09:25 +0100

    setup js

--- routes/splash.ts
+++ routes/splash.ts
@@ -2,8 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-	res.render('splash', { css: ['splash'] });
+	res.render('splash', { css: ['splash'], js: ['splash'] });
 });
-
 
 module.exports = router;
