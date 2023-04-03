5e7fb6d56d047e05c35027d888d28c7ff7002ee2
Laurens256 <duinlaurens2@gmail.com>
2023-04-03 12:22:59 +0200

    implement offline page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,38 @@
 const offlineCacheName = 'offline';
-const offlineCacheFiles = ['/styles/main.css', '/offline.html'];
+const offlineCacheFiles = [
+	'/styles/main.css',
+	'/styles/partials/header-footer.css',
+	'/offline.html',
+	'/manifest.json',
+	'/fonts/pokemon-fire-red.woff2',
+	'/img/favicon.png',
+];
 
 self.addEventListener('install', (event) => {
-	console.log('install');
-	event.waitUntil(addToCache(offlineCacheName, offlineCacheFiles));
+	event.waitUntil(
+		(async () => {
+			const cache = await caches.open(offlineCacheName);
+			await cache.addAll(offlineCacheFiles);
+		})()
+	);
 });
 
-const addToCache = async (cacheName, resources) => {
-	const cache = await caches.open(cacheName);
-	await cache.addAll(resources);
-};
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', async (event) => {
 	event.respondWith(
 		fetch(event.request)
 			.then((response) => {
 				return response;
 			})
 			.catch(async () => {
-				return caches.match('/fallback.html');
+				// If there's no network, serve the cached response
+				const response = await caches.match(event.request);
+				if (response) {
+					return response;
+				} else {
+					// If there's no cached response, serve the fallback HTML page
+					return caches.match('/offline.html');
+				}
 			})
 	);
 });
