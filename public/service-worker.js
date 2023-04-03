3a4a1121d3e321e37c3c987322ce5aab70f0c940
Laurens256 <duinlaurens2@gmail.com>
2023-04-03 09:21:38 +0200

    update sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,42 +1,31 @@
-const CACHE_NAME = 'my-cache';
-// const urlsToCache = ['/', '/index.html', '/style.css', '/script.js'];
+const offlineCacheName = 'offline';
+const offlineCacheFiles = ['/styles/main.css', '/offline.html'];
 
-self.addEventListener('install', async (event) => {
-	// const cache = await caches.open(CACHE_NAME);
-	// await cache.addAll(urlsToCache);
-	await self.skipWaiting();
+self.addEventListener('install', (event) => {
+	console.log('install');
+	event.waitUntil(addToCache(offlineCacheName, offlineCacheFiles));
 });
 
-self.addEventListener('activate', async (event) => {
-	const cacheNames = await caches.keys();
-	await Promise.all(
-		cacheNames
-			.filter((cacheName) => cacheName !== CACHE_NAME)
-			.map((cacheName) => caches.delete(cacheName))
+const addToCache = async (cacheName, resources) => {
+	const cache = await caches.open(cacheName);
+	await cache.addAll(resources);
+};
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		fetch(event.request)
+			.then((response) => {
+				return response;
+			})
+			.catch(async () => {
+				// If there's no network, serve the cached response
+				// const response = await caches.match(event.request);
+				// if (response) {
+				// 	return response;
+				// } else {
+					// If there's no cached response, serve the fallback HTML page
+					return caches.match('/fallback.html');
+				// }
+			})
 	);
-	await self.clients.claim();
-});
-
-self.addEventListener('fetch', async (event) => {
-	const response = await caches.match(event.request);
-	if (response) {
-		console.log('returned from cache', response);
-		return response;
-	}
-	try {
-		const networkResponse = await fetch(event.request);
-		console.log(networkResponse, networkResponse.status, networkResponse.type);
-		if (
-			networkResponse &&
-			networkResponse.status === 200 &&
-			networkResponse.type === 'basic'
-		) {
-			const cache = await caches.open(CACHE_NAME);
-			await cache.put(event.request, networkResponse.clone());
-		}
-		return networkResponse;
-	} catch (error) {
-		console.error('Fetch failed:', error);
-		throw error;
-	}
 });
