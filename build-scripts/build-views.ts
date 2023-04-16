73ee6160e8c789684bb94a7021dfd1f2029cf95e
Laurens256 <duinlaurens2@gmail.com>
2023-04-16 20:39:23 +0200

    cleanup and enable sw

--- build-scripts/build-views.ts
+++ build-scripts/build-views.ts
@@ -1,17 +1,5 @@
 import gulp from 'gulp';
-import htmlmin from 'gulp-htmlmin';
 
 (() => {
-	return gulp
-		.src('views/**/*.hbs', { base: './' })
-		.pipe(
-			htmlmin({
-				collapseWhitespace: true,
-				removeComments: true,
-				minifyJS: true,
-				minifyCSS: true,
-				ignoreCustomFragments: [/{{[\s\S]*?}}/]
-			})
-		)
-		.pipe(gulp.dest('./dist/'));
+	return gulp.src('views/**/*.hbs', { base: './' }).pipe(gulp.dest('./dist/'));
 })();
